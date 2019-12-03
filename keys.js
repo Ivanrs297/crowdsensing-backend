c3a77125f1c25f4a7e45508d09c1ec9c2464bd99
Ivan Reyes <ivanrs297@gmail.com>
2019-12-03 15:26:13 -0600

    Adding DB

--- keys.js
+++ keys.js
@@ -1,8 +1,15 @@
 module.exports = {
+    // database: {
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: '',
+    //     database: 'database_crowdsensing'
+    // }
+
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'database_crowdsensing'
+        host: 'us-cdbr-iron-east-05.cleardb.net',
+        user: 'bfe28eb0ef2699',
+        password: '871211a6',
+        database: 'heroku_f0851cd0e4a3bee'
     }
 }
