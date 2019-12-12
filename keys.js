df2376ef23f52f07c25dafe92e8b465228c03ad0
Ivan Reyes <ivanrs297@gmail.com>
2019-12-12 16:54:53 -0600

    Adding AWS DB

--- keys.js
+++ keys.js
@@ -6,10 +6,17 @@ module.exports = {
     //     database: 'database_crowdsensing'
     // }
 
+    // database: {
+    //     host: 'us-cdbr-iron-east-05.cleardb.net',
+    //     user: 'bfe28eb0ef2699',
+    //     password: '871211a6',
+    //     database: 'heroku_f0851cd0e4a3bee'
+    // }
+
     database: {
-        host: 'us-cdbr-iron-east-05.cleardb.net',
-        user: 'bfe28eb0ef2699',
-        password: '871211a6',
-        database: 'heroku_f0851cd0e4a3bee'
+        host: 'slashwebmariadb.cyuazzw9rdsu.us-east-1.rds.amazonaws.com',
+        user: 'geosensing',
+        password: '8vfYLf2j9BXPbtUu',
+        database: 'geosensing'
     }
 }
